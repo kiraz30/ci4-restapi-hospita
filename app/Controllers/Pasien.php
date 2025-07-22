d994afc27354a29a395187dc8ff168dc7be4d0ad
Bima Setya Nugraha <setyabima016@gmail.com>
2025-07-22 19:47:15 +0700

    Add Fitur : Get data by id, Update data by id , Delete data by id

--- app/Controllers/Pasien.php
+++ app/Controllers/Pasien.php
@@ -27,6 +27,19 @@ class Pasien extends ResourceController
     public function show($id = null)
     {
         //get date by id
+
+        $data = $this->model->find($id);
+
+        if (!$data){
+            return $this->respond([
+                'status' =>false,
+                'message' => 'Data pasien tidak ditemukan',
+                ], 404);
+        }
+        return $this->respond([
+            'status' => true,
+            'data' =>$data,
+        ],200);
     }
 
   
@@ -62,18 +75,56 @@ class Pasien extends ResourceController
  
     public function update($id = null)
     {
-        //update data pasien
+        //update data pasien by id
+        if ($this->model->find($id) === null){
+            return $this->respond([
+                'status' =>false,
+                'message' => 'Data pasien tidak ditemukan',
+                ], 404);
+        }
+
+        $this->model->setValidationRule('nik','required|is_unique[pasien.nik,id,'.$id.']');
+        $this->model->setValidationRule('no_hp','required|is_unique[pasien.no_hp,id,'.$id.']');
+        $this->model->setValidationRule('email','required|is_unique[pasien.email,id,'.$id.']');
+         
+        $data = [
+            'id' => $id,
+            'nama_pasien' =>$this->request->getVar('nama_pasien'),
+            'tanggal_lahir'=> $this->request->getVar('tanggal_lahir'),
+            'alamat' => $this->request->getVar('alamat'),
+            'nik' => $this->request->getVar('nik'),
+            'no_hp' => $this->request->getVar('no_hp'),
+            'email' => $this->request->getVar('email'),
+        ];
+
+        if ($this->model->update($id, $data)){
+            return $this->respond([
+                'status'=> true,
+                'message'=> 'Data pasien berhasil diupdate'
+            ], 200);
+        } else {
+            return $this->respond([
+                'status'=> false,
+                'errors'=> $this->model->errors()
+            ], 400);
+        }
     }
 
-    /**
-     * Delete the designated resource object from the model.
-     *
-     * @param int|string|null $id
-     *
-     * @return ResponseInterface
-     */
+   
     public function delete($id = null)
     {
         //delete data pasien
+        if ($this->model->find($id) === null){
+            return $this->respond([
+                'status' =>false,
+                'message' => 'Data pasien tidak ditemukan',
+                ], 404);
+        }
+        $this->model->delete($id);
+        return $this->respond([
+            'status'=> true,
+            'message'=> 'Data pasien berhasil dihapus'
+        ], 200);
+
     }
 }
